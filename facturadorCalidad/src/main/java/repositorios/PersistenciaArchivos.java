897bac0b78b50bbaf8db2af36f19147c338fb5f3
Diego <DiegoViscarra@users.github.com>
2020-09-25 21:33:25 -0400

    PersistenciaArchivos
    Use try-with-resources or close this "FileOutputStream" in a "finally" clause.
    Use try-with-resources or close this "ObjectOutputStream" in a "finally" clause.

--- facturadorCalidad/src/main/java/repositorios/PersistenciaArchivos.java
+++ facturadorCalidad/src/main/java/repositorios/PersistenciaArchivos.java
@@ -24,11 +24,13 @@ public class PersistenciaArchivos implements IPersistenciaArchivos {
             File documento = new File(fechaDocumento + ".txt"); 
             String rutaActual = rutaCarpeta + documento.toString();
          
-            try(FileOutputStream archivoDeSalida = new FileOutputStream((rutaActual));
-			ObjectOutputStream objetoDeSalida = new ObjectOutputStream(archivoDeSalida);){
-			objetoDeSalida.writeObject(registrosTelefonicos);
-			objetoDeSalida.close();
-			archivoDeSalida.close();}
+            try(
+        		FileOutputStream archivoDeSalida = new FileOutputStream((rutaActual));
+				ObjectOutputStream objetoDeSalida = new ObjectOutputStream(archivoDeSalida);){
+				objetoDeSalida.writeObject(registrosTelefonicos);
+				objetoDeSalida.close();
+				archivoDeSalida.close();
+			}
 		} catch (IOException ex) {
 			System.out.println("Exception caught");
 		}	
@@ -45,13 +47,14 @@ public class PersistenciaArchivos implements IPersistenciaArchivos {
 		try {
 			File documento = new File(nombreArchivo); 
 			String rutaActual = rutaCarpeta + documento.toString();
-			FileInputStream archivoEntrada = new FileInputStream(rutaActual);
-			ObjectInputStream objetoEntrada = new ObjectInputStream(archivoEntrada);
-			
-			ArrayList<CDR> lecturaRegistros = new ArrayList<CDR>();
-			lecturaRegistros = (ArrayList<CDR>)objetoEntrada.readObject();
-			objetoEntrada.close();
-			return lecturaRegistros;
+			try(
+				FileInputStream archivoEntrada = new FileInputStream(rutaActual);
+				ObjectInputStream objetoEntrada = new ObjectInputStream(archivoEntrada);){
+				ArrayList<CDR> lecturaRegistros = new ArrayList<CDR>();
+				lecturaRegistros = (ArrayList<CDR>)objetoEntrada.readObject();
+				objetoEntrada.close();
+				return lecturaRegistros;
+			}
 		} catch (IOException ex) {
 			System.out.println("Exception caught");
 		}
