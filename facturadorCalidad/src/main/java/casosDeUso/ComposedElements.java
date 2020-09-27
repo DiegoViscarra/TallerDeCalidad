9a15d41abd8ce37cfd66b480104c7abd7e25099d
Widni Galindo <g3.wth17@gmail.com>
2020-09-27 17:45:33 -0400

    codeSmells/major/fixed/ComposedElements

--- facturadorCalidad/src/main/java/casosDeUso/ComposedElements.java
+++ facturadorCalidad/src/main/java/casosDeUso/ComposedElements.java
@@ -3,4 +3,8 @@ package casosDeUso;
 public final class ComposedElements {
 	public static ObtenerMontoTarifa tarifa = new ObtenerMontoTarifa();
 	public static CalculadorCostoLlamada costoLlamada = new CalculadorCostoLlamada();
+	
+	private ComposedElements() {
+		
+	}
 }
