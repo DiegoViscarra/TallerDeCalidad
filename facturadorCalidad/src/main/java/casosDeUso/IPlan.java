a48c2bf5dea76ea75e3dc1a2a38e18fd3974a3ab
Widni Galindo <g3.wth17@gmail.com>
2020-09-27 17:49:54 -0400

    codeSmells/major/fixed/IPlan

--- facturadorCalidad/src/main/java/casosDeUso/IPlan.java
+++ facturadorCalidad/src/main/java/casosDeUso/IPlan.java
@@ -3,8 +3,6 @@ package casosDeUso;
 import entidades.CDR;
 
 public interface IPlan {
-	//public static ObtenerMontoTarifa tarifa = new ObtenerMontoTarifa();
-	//public static CalculadorCostoLlamada costoLlamada = new CalculadorCostoLlamada();
 	double calcularCostoDeUnaLlamada(CDR registro);
 	String obtenerTipoTarifa();
 }
