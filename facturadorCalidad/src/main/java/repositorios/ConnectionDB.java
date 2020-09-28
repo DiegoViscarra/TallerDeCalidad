22684f44dc6b0d842ffe63f0bb755454e991b3d8
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-09-27 21:58:27 -0400

    Bug/fixed/ConnectionDB

--- facturadorCalidad/src/main/java/repositorios/ConnectionDB.java
+++ facturadorCalidad/src/main/java/repositorios/ConnectionDB.java
@@ -1,31 +1,44 @@
 package repositorios;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.Scanner;
+
+import entidades.User;
 
 public class ConnectionDB  {
 
 	private static final String CONNETTION_DB = "jdbc:sqlite:dbSQL.db;";
-	
+	private static String rutaCarpeta = Paths.get(".").toAbsolutePath().normalize().toString();
+
 	public String getConnection() {
-		String [] user1 = getUserAndPassFromTXT();
-		String CONNETTION_DB_WHIT_USER = CONNETTION_DB + "user=" + user1[0] + "&password=" + user1[1];
+		 User user = getUserAndPassFromTXT();
+		String CONNETTION_DB_WHIT_USER = CONNETTION_DB + "user=" + user.getName() + "&password=" + user.getPassword();
 		return CONNETTION_DB_WHIT_USER;
 	}
 	
 	
-	private String [] getUserAndPassFromTXT() {
-		 try {
-		      File myObj = new File("Users.txt");
-		      Scanner myReader = new Scanner(myObj);
-		        String data = myReader.nextLine();
-		      myReader.close();
-		      return data.split("\\s+");
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		return null;
+	private User getUserAndPassFromTXT() {
+		try {
+			File file = new File(rutaCarpeta+"\\Users.txt");
+			try (Scanner input = new Scanner(file);){
+				input.useDelimiter("-|\n");
+			    String nombre = "";
+			    String password = "";
+			    nombre = input.next();
+			    nombre = input.next();
+			    input.close();
+			    User user = new User(nombre, password);
+	            return user;
+			}
+			
+		}
+			catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return new User("","");
+		}	
+		
 	}
 	
 }
