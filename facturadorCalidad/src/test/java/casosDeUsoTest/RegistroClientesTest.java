727ffafe0d85246dcf2b11678a5bfaf1ae0d0f5d
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-11-13 03:22:55 -0400

    se implemeto borrar persistencia

--- facturadorCalidad/src/test/java/casosDeUsoTest/RegistroClientesTest.java
+++ facturadorCalidad/src/test/java/casosDeUsoTest/RegistroClientesTest.java
@@ -2,6 +2,7 @@ package casosDeUsoTest;
 
 import java.util.ArrayList;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,17 +24,20 @@ import repositorios.RepositorioCliente;
 
 public class RegistroClientesTest {
 	RegistroClientes registroClientes;
+	PersistenciaBDClientes persistenciaClientes;
+	PersistenciaBDCDR persistenciaBDCDR ;
+
 	@Test
 	public void guardarTemporalmenteCDRs() {
 		registroClientes.guardarClientesCargados("788;456;Sergio Ramos;Postpago;[]\r\n");
-		Assert.assertEquals(registroClientes.buscarCliente(456).getNumeroTelefonico(),456);
+		Assert.assertEquals(registroClientes.buscarCliente(456).getNumeroTelefonico(), 456);
 	}
 
 	@Test
 	public void guardarClientesCargados() {
 		registroClientes.guardarClientesCargados("788;456;Sergio Ramos;Postpago;[]\r\n");
 		ArrayList<Cliente> resultado = registroClientes.devolverClientes();
-	
+
 		Assert.assertEquals(resultado.get(0).getCi(), "788");
 	}
 
@@ -44,7 +48,7 @@ public class RegistroClientesTest {
 		ArrayList<Cliente> resultado = registroClientes.devolverClientes();
 		Assert.assertEquals(resultado.get(0).getCi(), "788");
 	}
-	
+
 	@Test
 	public void llenarListaClientesPlanWow() {
 		registroClientes.guardarClientesCargados("788;456;Sergio Ramos;Wow;[234,234,456,567]\r\n");
@@ -52,7 +56,7 @@ public class RegistroClientesTest {
 		ArrayList<Cliente> resultado = registroClientes.devolverClientes();
 		Assert.assertEquals(resultado.get(0).getCi(), "788");
 	}
-	
+
 	@Test
 	public void noCoincidenEnNumeroTelefonico() {
 		registroClientes.guardarClientesCargados("788;321;Sergio Ramos;Wow;[234,423,456,567]\r\n");
@@ -60,17 +64,25 @@ public class RegistroClientesTest {
 		ArrayList<Cliente> resultado = registroClientes.devolverClientes();
 		Assert.assertEquals(resultado.get(0).getCi(), "788");
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() {
 		IRepositorioCDR repositorioCDR = new RepositorioCDR();
-		IPersistenciaBDClientes persistenciaClientes = new PersistenciaBDClientes();
-		PersistenciaBDCDR persistenciaBDCDR = new PersistenciaBDCDR();
+		persistenciaClientes = new PersistenciaBDClientes();
+		persistenciaBDCDR = new PersistenciaBDCDR();
 		persistenciaBDCDR.borrarTodosLosDatosDeUsuario();
 		IPersistencia persistencia = new Persistencia(persistenciaBDCDR, persistenciaClientes,
 				new PersistenciaArchivos(), repositorioCDR);
-		
 		IRepositorioCliente repositorioCliente = new RepositorioCliente(persistencia);
 		registroClientes = new RegistroClientes(repositorioCliente, persistenciaClientes);
 	}
+
+	@AfterClass
+	public void borrarRegistrosClientes() {
+		persistenciaClientes.borrarTodosLosDatosDeClientes();
+		persistenciaClientes.borrarTodosLosDatosDeNumerosAmigos();
+		persistenciaBDCDR.borrarTodosLosDatosDeCDR();
+		persistenciaBDCDR.borrarTodosLosDatosDeUsuario();
+	}
+
 }
