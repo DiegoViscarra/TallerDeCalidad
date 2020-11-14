9249afe34cd0f9133952768f1edc94d3ad80859d
Widni Galindo <g3.wth17@gmail.com>
2020-11-14 02:08:38 -0400

    Persistencia 60%, ObtenerMontoTarifaLlamada 100%

--- facturadorCalidad/src/main/java/casosDeUso/ObtenerMontoTarifa.java
+++ facturadorCalidad/src/main/java/casosDeUso/ObtenerMontoTarifa.java
@@ -30,7 +30,7 @@ public class ObtenerMontoTarifa {
 		}
 	}
 
-	private double obtenerMontoTarifaDiferenciadaPorHora(CDR registro) {
+	public double obtenerMontoTarifaDiferenciadaPorHora(CDR registro) {
 		double tarifa = 0;
 		int hora = Integer.parseInt(registro.getHora().split(":")[0]);
 		if(estaEnHorarioNormal(hora))
@@ -42,23 +42,23 @@ public class ObtenerMontoTarifa {
 		return tarifa;
 	} 
 
-	private double obtenerMontoTarifaWow() {
+	public double obtenerMontoTarifaWow() {
 		return TARIFANORMALWOW;
 	}
 
-	private double obtenerMontoTarifaPostpago() {
+	public double obtenerMontoTarifaPostpago() {
 		return TARIFAFIJAPOSTPAGO;
 	}
 	
-	private boolean estaEnHorarioSuperReducido(int hora) {
+	public boolean estaEnHorarioSuperReducido(int hora) {
 		return hora >=1 && hora < 7;
 	}
 
-	private boolean estaEnHorarioReducido(int hora) {
+	public boolean estaEnHorarioReducido(int hora) {
 		return hora >= 21 && hora < 24 || hora >= 0 && hora < 1;
 	}
 
-	private boolean estaEnHorarioNormal(int hora) {
+	public boolean estaEnHorarioNormal(int hora) {
 		return hora >= 7 && hora < 21;
 	}
 }
