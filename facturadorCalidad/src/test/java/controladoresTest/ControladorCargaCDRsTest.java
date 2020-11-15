447fc77b54acab501c474d78316a73de178f6f08
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-11-15 12:17:23 -0400

    se implemento tests con apache

--- facturadorCalidad/src/test/java/controladoresTest/ControladorCargaCDRsTest.java
+++ facturadorCalidad/src/test/java/controladoresTest/ControladorCargaCDRsTest.java
@@ -6,64 +6,72 @@ import casosDeUso.IRegistroCDR;
 import casosDeUso.IRepositorioCDR;
 import casosDeUso.RegistroCDR;
 import controladores.ControladorCargaCDRs;
+import jdk.jfr.ContentType;
 import repositorios.RepositorioCDR;
 
 import static spark.Spark.port;
 import static spark.Spark.stop;
 
+import java.io.File;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class ControladorCargaCDRsTest {
-	HttpClient client;
-	HttpRequest request;
-	HttpResponse<?> response;
 	IRegistroCDR registroCDR;
+	IRepositorioCDR repositorioCDR;
 
 	@Test
 	public void getRegistrosCargados() throws Exception, InterruptedException {
-		HttpClient client = HttpClient.newHttpClient();
-		request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/registrosCargados")).build();
-		response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		Assert.assertEquals(response.statusCode(), 200);
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet("http://localhost:8080/registrosCargados");
+		HttpResponse response = client.execute(request);
+		Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
 	}
 
 	@Test
 	public void postApiSubmit() throws Exception, InterruptedException {
-		Path data = Paths.get(
-				"D:\\II-2020\\GESTION DE CALIDAD DE SISTEMAS (CANEDO ARZE CARLOS OSWALDO)\\Practica 3\\2\\facturadorCalidad\\src\\test\\java\\controladoresTest\\DatosParaCargar.csv");
-		request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/submit"))
-				.POST(BodyPublishers.ofFile(data)).build();
-		request.uri().getRawPath();
-		response = client.send(request, BodyHandlers.discarding());
-		Assert.assertEquals(response.statusCode(), 200);
-		Assert.assertEquals(3, registroCDR.obtenerRegistrosNoTarificados().size());
+		repositorioCDR.vaciarRegistros();
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost("http://localhost:8080/api/submit");
+		File uploadFile = new File("FilesTest/DatosParaCargar.csv");
+		FileBody uploadFilePart = new FileBody(uploadFile);
+		MultipartEntity reqEntity = new MultipartEntity();
+		reqEntity.addPart("myFile", uploadFilePart);
+		httpPost.setEntity(reqEntity);
+
+		HttpResponse response = httpclient.execute(httpPost);
+		Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+		Assert.assertEquals(7, registroCDR.obtenerRegistrosNoTarificados().size());
 	}
 
 	@Test
 	public void postApiSubmitSinArchivo() throws Exception, InterruptedException {
-		request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/submit"))
-				.POST(BodyPublishers.noBody()).build();
-		request.uri().getRawPath();
-		response = client.send(request, BodyHandlers.discarding());
-		Assert.assertEquals(response.statusCode(), 200);
+		repositorioCDR.vaciarRegistros();
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost("http://localhost:8080/api/submit");
+		HttpResponse response = httpclient.execute(httpPost);
+		Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
 		Assert.assertEquals(0, registroCDR.obtenerRegistrosNoTarificados().size());
 	}
 
 	@BeforeClass
 	public void beforeClass() {
-		IRepositorioCDR repositorioCDR = new RepositorioCDR();
+		repositorioCDR = new RepositorioCDR();
+
 		registroCDR = new RegistroCDR(repositorioCDR);
 		port(8080);
 		new ControladorCargaCDRs(registroCDR);
@@ -71,10 +79,9 @@ public class ControladorCargaCDRsTest {
 
 	@BeforeMethod
 	public void beforeMethod() {
-		client = HttpClient.newBuilder().build();
-		response = null;
+
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		stop();
